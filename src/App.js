051e227c544be9054f8f0eeedbf7c6b145397e3d
Nishiyama13 <nishiyamaeel@gmail.com>
2022-11-24 01:08:30 -0300

    versao layout colorido

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 function App() {
   return (
     <>
-      <div className="container">
-        <div className="esquerdaContainer">
-          <img className="imagemForca" src=./assets/forca3.png" alt="forca 3" />
+      <main>
+        <div className="containerGame">
+          <div className="esquerdaContainer">
+            <img
+              className="imagemForca"
+              src="/assets/forca3.png"
+              alt="forca 3"
+            />
+          </div>
+          <div className="direitaContainer">
+            <button className="botaoEscolher">Escolher Palavra</button>
+            <div className="palavra">_ _ _ _ _ _ _ _ _ _</div>
+          </div>
         </div>
-        <div className="direitaContainer">
-          <button className="botaoEscolher">Escolher Palavra</button>
+        <div className="teclado">
+          <button className="tecla">A</button>
+          <button className="tecla">B</button>
+          <button className="tecla">C</button>
+          <button className="tecla">D</button>
+          <button className="tecla">E</button>
+          <button className="tecla">F</button>
+          <button className="tecla">G</button>
+          <button className="tecla">H</button>
+          <button className="tecla">I</button>
+          <button className="tecla">J</button>
+          <button className="tecla">K</button>
+          <button className="tecla">L</button>
+          <button className="tecla">M</button>
+          <button className="tecla">N</button>
+          <button className="tecla">O</button>
+          <button className="tecla">P</button>
+          <button className="tecla">Q</button>
+          <button className="tecla">R</button>
+          <button className="tecla">S</button>
+          <button className="tecla">T</button>
+          <button className="tecla">U</button>
+          <button className="tecla">V</button>
+          <button className="tecla">W</button>
+          <button className="tecla">X</button>
+          <button className="tecla">Y</button>
+          <button className="tecla">Z</button>
         </div>
-      </div>
+        <div className="containerResposta">
+          <p>Já sei a palavra!</p>
+          <input
+            className="inputPalavra"
+            type="text"
+            name="input"
+            value="Digite a palavra..."
+          ></input>
+          <button className="botaoChute">Chutar!</button>
+        </div>
+      </main>
     </>
   );
 }

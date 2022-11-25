983c372896a0c79728e58e245e47190b069380f8
Nishiyama13 <nishiyamaeel@gmail.com>
2022-11-25 14:20:17 -0300

    placeholder e limpo ao chutar

--- src/Chute.js
+++ src/Chute.js
@@ -1,16 +1,30 @@
+import { useState } from "react";
+
 export default function Chute(props) {
+  const [chute, setChute] = useState([]);
+  function testarChute() {
+    //logica de comparacao
+    setChute("");
+  }
+
   return (
     <>
       <div className="containerResposta">
         <p>Já sei a palavra!</p>
         <input
+          placeholder="Digite a palavra..."
           className="inputPalavra"
           data-test="guess-input"
           type="text"
           name="input"
-          value="Digite a palavra..."
+          onChange={e => setChute(e.target.value)}
+          value={chute}
         ></input>
-        <button className="botaoChute" data-test="guess-button">
+        <button
+          className="botaoChute"
+          data-test="guess-button"
+          onClick={() => testarChute()}
+        >
           Chutar!
         </button>
       </div>
